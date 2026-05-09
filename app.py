9bf19012879d6656616f11efa02df93b13b7345b
elpecao3444h <reynamejiaalexis67@gmail.com>
2026-05-08 17:45:20 -0700

    este es mi primer avanze

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template,request
+
+app = Flask(__name__)
+
+#ruta principal 
+@app.route('/')
+def supermarket():
+    return render_template('supermarket.html')
+
+
+@app.route('/compras')
+def comprar():
+    return render_template('compras.html')
+
+    
+# Ejecutar la app
+if __name__ == "__main__":
+    app.run(debug=True)
