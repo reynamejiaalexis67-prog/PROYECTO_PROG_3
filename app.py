0b83ca20ff8adae474d296cf33e3eaa832bd6776
elpecao3444h <reynamejiaalexis67@gmail.com>
2026-05-12 12:01:32 -0700

    archivo actualizado

--- app.py
+++ app.py
@@ -7,10 +7,20 @@ app = Flask(__name__)
 def supermarket():
     return render_template('supermarket.html')
 
-
+#creamos nuestras rutas secundarias de compras
 @app.route('/compras')
 def comprar():
     return render_template('comprar.html')
+
+
+
+#creamos una segunda ruta secundaria para agregar productos 
+@app.route('/agregar')
+def agregar():
+    return render_template('agregar.html')
+
+
+
 
     
 # Ejecutar la app
